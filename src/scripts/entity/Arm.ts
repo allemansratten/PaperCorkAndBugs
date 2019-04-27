161f5a3076e6ca1d197e874a1504a567deb0b692
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-04-27 16:32:05 +0200

    add moving legs

--- src/scripts/entity/Arm.ts
+++ src/scripts/entity/Arm.ts
@@ -4,13 +4,23 @@ import {Vector} from "vector2d"
 export class Arm implements Drawable {
 
     pos : Vector
+    dir : number
+    defaultDir : number
 
-    constructor(pos: Vector) {
+    constructor(pos: Vector, defaultDir: number) {
         this.pos = pos
+        this.defaultDir = defaultDir
     }
 
     draw(context: CanvasRenderingContext2D): void {
+        context.save()
+        context.translate(this.pos.x, this.pos.y)
+        context.rotate(this.defaultDir*Math.PI*2/8)
         context.fillStyle = "black"
-        context.fillRect(this.pos.x, this.pos.y, 10, 2)
+        context.beginPath()
+        context.fillRect(0,-2, 4, 10)
+        // context.ellipse(6, 9, 6, 2, 0, 2*Math.PI, 2*Math.PI*3/4, false)
+        // context.fill()
+        context.restore()
     }
 }
--- src/scripts/entity/Leg.ts
+++ src/scripts/entity/Leg.ts
@@ -1,16 +1,29 @@
 import {Drawable} from "../Drawable"
 import {Vector} from "vector2d"
+import {Player} from "./Player"
 
 export class Leg implements Drawable {
 
     pos : Vector
+    rot : number
+    speed : number
+    private baseWiggleOffset : number = Math.random()*10
 
     constructor(pos: Vector) {
         this.pos = pos
     }
 
     draw(context: CanvasRenderingContext2D): void {
+        let time = Date.now()
+
+        context.save()
+        context.translate(this.pos.x, this.pos.y)
+        context.rotate(this.rot+Math.sin(this.baseWiggleOffset + time/100)*this.speed/Player.MAX_SPEED)
         context.fillStyle = "black"
-        context.fillRect(this.pos.x, this.pos.y, 2, 10)
+        context.beginPath()
+        context.fillRect(0,-2, 4, 10)
+        context.ellipse(6, 9, 6, 2, 0, 2*Math.PI, 2*Math.PI*3/4, false)
+        context.fill()
+        context.restore()
     }
 }
