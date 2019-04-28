012a5cdb8c391ddfd6c168c03c1a18f167a3a1e9
Jan Boucek <jan.boucek.lol@gmail.com>
2019-04-28 17:20:37 +0200

    slower monster addition

--- src/scripts/entity/MonsterGenerator.ts
+++ src/scripts/entity/MonsterGenerator.ts
@@ -11,7 +11,7 @@ import {Ladybug} from "./monster/Ladybug"
 
 export class MonsterGenerator {
     private static readonly FIRST_LEVEL_MONSTERS = 5
-    private static readonly LEVEL_MONSTERS_INCREMENT = 1
+    private static readonly LEVEL_MONSTERS_INCREMENT = 0.4
     private static readonly NEW_MONSTER_MIN_DIST = 250
     private static readonly LEVEL_MONSTERS = [
         [Ant],
