5e34e90be285da7dc0e8eab1af1eddf126e98d01
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-04-28 16:11:07 +0200

    Do not draw eye using circles

--- src/scripts/entity/Eye.ts
+++ src/scripts/entity/Eye.ts
@@ -27,42 +27,42 @@ export class Eye extends BodyPart implements Drawable {
     }
 
     draw(context: CanvasRenderingContext2D): void {
-        let time = Date.now()
-
-        context.fillStyle = Eye.WHITE_COLOR
-        context.beginPath()
-        context.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI)
-        context.fill()
-        context.fillStyle = Eye.IRIS_COLOR
-
-        context.beginPath()
-        context.arc(this.pos.x, this.pos.y, this.r / 2, 0, 2 * Math.PI)
-        context.closePath()
-        context.fill()
-
-        // roll dice to start blinking
-        if (this.blinkingState == -1) {
-            // this is so that the probs wouldn't be frame dependent
-            if (Math.random() > 1 - this.blinkingLastTime / time / Eye.BLINKING_INTERVAL) {
-                this.blinkingState = 0
-                this.blinkingSinePrev = 0
-                this.blinkingStartTime = this.blinkingLastTime = Date.now()
-            }
-        } else {
-            let blinkSine = Math.sin((this.blinkingStartTime - time) / Eye.BLINKING_DURATION)
-            context.beginPath()
-            // context.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * (1-blinkSine))
-            context.ellipse(this.pos.x, this.pos.y, this.r, (1 + blinkSine) * this.r, 0, 0, 2 * Math.PI)
-            context.fill()
-            context.fillStyle = Eye.EYELID_COLOR
-            if (this.blinkingState == 0 && blinkSine <= this.blinkingSinePrev) {
-                this.blinkingState = 1
-            }
-            if (this.blinkingState == 1 && blinkSine >= this.blinkingSinePrev) {
-                this.blinkingState = -1
-            }
-            this.blinkingSinePrev = blinkSine
-        }
+        // let time = Date.now()
+        //
+        // context.fillStyle = Eye.WHITE_COLOR
+        // context.beginPath()
+        // context.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI)
+        // context.fill()
+        // context.fillStyle = Eye.IRIS_COLOR
+        //
+        // context.beginPath()
+        // context.arc(this.pos.x, this.pos.y, this.r / 2, 0, 2 * Math.PI)
+        // context.closePath()
+        // context.fill()
+        //
+        // // roll dice to start blinking
+        // if (this.blinkingState == -1) {
+        //     // this is so that the probs wouldn't be frame dependent
+        //     if (Math.random() > 1 - this.blinkingLastTime / time / Eye.BLINKING_INTERVAL) {
+        //         this.blinkingState = 0
+        //         this.blinkingSinePrev = 0
+        //         this.blinkingStartTime = this.blinkingLastTime = Date.now()
+        //     }
+        // } else {
+        //     let blinkSine = Math.sin((this.blinkingStartTime - time) / Eye.BLINKING_DURATION)
+        //     context.beginPath()
+        //     // context.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI * (1-blinkSine))
+        //     context.ellipse(this.pos.x, this.pos.y, this.r, (1 + blinkSine) * this.r, 0, 0, 2 * Math.PI)
+        //     context.fill()
+        //     context.fillStyle = Eye.EYELID_COLOR
+        //     if (this.blinkingState == 0 && blinkSine <= this.blinkingSinePrev) {
+        //         this.blinkingState = 1
+        //     }
+        //     if (this.blinkingState == 1 && blinkSine >= this.blinkingSinePrev) {
+        //         this.blinkingState = -1
+        //     }
+        //     this.blinkingSinePrev = blinkSine
+        // }
 
         const img = ImageManager.get("eye1")
         context.drawImage(img, 0, 0, img.width, img.height,
