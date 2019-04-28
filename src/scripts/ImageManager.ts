0a9a4ffba340af7164992a54ff68d90210e36246
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-04-28 10:46:27 +0200

    Add simple image support

---
+++ src/scripts/ImageManager.ts
@@ -0,0 +1,14 @@
+export class ImageManager {
+
+    private static images : Map<string, HTMLImageElement> = new Map<string, HTMLImageElement>()
+
+    public static add(name: string, url: string) {
+        let img = new Image()
+        img.src = url
+        ImageManager.images.set(name, img)
+    }
+
+    public static get(name: string) {
+        return ImageManager.images.get(name)
+    }
+}
--- src/scripts/Level.ts
+++ src/scripts/Level.ts
@@ -1,6 +1,7 @@
 import {Drawable} from "./Drawable"
 import {Player} from "./entity/Player"
 import {Tile} from "./Tile"
+import {ImageManager} from "./ImageManager"
 
 export class Level implements Drawable {
 
@@ -22,12 +23,18 @@ export class Level implements Drawable {
         for (let xi = 0; xi < this.width; xi++) {
             for (let yi = 0; yi < this.height; yi++) {
                 const tile = this.tiles[xi][yi]
+                const img = ImageManager.get("harold")
+                context.drawImage(img, 0, 0, img.width, img.height,
+                    xi * Level.TILE_SIZE, yi * Level.TILE_SIZE, Level.TILE_SIZE+4, Level.TILE_SIZE+4)
+                context.globalAlpha = 0.8
                 if (tile.obstacle) {
                     context.fillStyle = "rgb(50,50,50)"
+
                 } else {
                     context.fillStyle = "rgb(10, 100, 100)"
                 }
-                context.fillRect(xi * Level.TILE_SIZE, yi * Level.TILE_SIZE, Level.TILE_SIZE+4, Level.TILE_SIZE+4)
+                context.fillRect(xi * Level.TILE_SIZE, yi * Level.TILE_SIZE, Level.TILE_SIZE+1, Level.TILE_SIZE+1)
+                context.globalAlpha = 1
             }
         }
     }
